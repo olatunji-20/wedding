bdcd3120155f9568f4b713ca74b6b30f48deedcc
falana sheriff <falanasheriffdeen@gmail.com>
2023-02-17 12:09:23 +0100

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,31 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import Homepage from '../views/Homepage.vue'
+import Donate from '../views/Donate.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Homepage',
+    component: Homepage
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-    }
+    path: '/donate-page',
+    name: 'Donate',
+    component: Donate
   }
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+  scrollBehavior(stand, homepage, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    }
+    return {
+      left: 0, top: 0
+    }
+  }
 })
 
 export default router
---
+++ src/views/Homepage.vue
@@ -0,0 +1,30 @@
+<template>
+    <section>
+        <h1>Thanks for attending our wedding.</h1>
+        <p>Please click on the button below to donate.</p>
+        <router-link to="/donate-page"><button>Donate</button></router-link>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "Homepage",
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+h1 {
+    font-size: 40px;
+    color: tomato;
+}
+button {
+    width: 200px;
+    height: 50px;
+    border-radius: 5px;
+}
+</style>
